4584083a43ef5259682565ce808427230ddbdb12
akshay <akshaykatoch38@gmail.com>
2024-08-28 01:09:00 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false, // Disable minification to check if it resolves the issue
   images: {
     domains: ["cdn.dribbble.com"]
+  },
+  webpack: (config, { isServer }) => {
+    // Example of adding custom webpack configurations or debugging options
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false
+      };
+    }
+    return config;
   }
 }
 
