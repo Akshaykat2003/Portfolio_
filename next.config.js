450f5d7e9f635f9cf46aa0c3d366cd8063d515bb
akshay <akshaykatoch38@gmail.com>
2024-08-28 03:42:02 +0530

    added

--- next.config.js
+++ next.config.js
@@ -1,21 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: false, // Disable minification to check if it resolves the issue
+  swcMinify: false,
   images: {
     domains: ["cdn.dribbble.com"]
   },
-  webpack: (config, { isServer }) => {
-    // Example of adding custom webpack configurations or debugging options
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-        path: false,
-        os: false
-      };
-    }
-    return config;
-  }
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
